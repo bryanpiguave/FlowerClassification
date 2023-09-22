258417aedbed1188f780db2beda5f52d892d046c
bryanpiguave <bpiguave@espol.edu.ec>
2023-09-21 21:28:35 -0500

    adding function to decode image

--- src/training.py
+++ src/training.py
@@ -1,6 +1,20 @@
-import tensorflow 
+import tensorflow as tf
+import sys 
+sys.path.append("..")
+IMAGE_SIZE=[192,192]
+def decode_image(image_data):
+    image = tf.image.decode_jpeg(image_data, channels=3)
+    image = tf.cast(image, tf.float32) / 255.0  # convert image to floats in [0, 1] range
+    image = tf.reshape(image, [*IMAGE_SIZE, 3]) # explicit size needed for TPU
+    return image
+
 
 def main():
+    
+    file_pattern = "data\\train\\*.tfrec"  # You can use a wildcard to match multiple files
+    dataset = tf.data.TFRecordDataset(tf.io.gfile.glob(file_pattern))
+    print(dataset)
+
     return 
 
 
